9c0d93aaf930dddca4bb85a50875868b8d9759f5
Gabriel <gabriel4amaizu@gmail.com>
2021-01-26 20:00:17 +0900

    CoachList component and Vuex

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -7,5 +7,46 @@
             <button>refesh</button>
             <router-link to="/register">Register a Coach</router-link>
         </div>
+        <ul v-if="hasCoaches">
+            <coach-item v-for="coach in filteredCoaches" 
+                :key="coach.id"
+                :id="coach.id"
+                :firstName="coach.firstName"
+                :lastName="coach.lastName"
+                :rate="coach.hourlyRate"
+                :areas="coach.areas">
+            </coach-item>
+        </ul>
+        <h3 v-else>No Coaches Found</h3>
     </section>
 </template>
+
+<script>
+import CoachItem from "../../components/coaches/CoachItem.vue";
+export default {
+    components: {
+        CoachItem,
+    },
+    computed: {
+        filteredCoaches() {
+            return this.$store.getters['coaches/coaches'];
+        },
+        hasCoaches() {
+            return this.$store.getters['coaches/hasCoaches'];
+        }
+    }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
